70cd813ad33033e455b2c8dde7eb89279ecff2c9
paulohenriquex <paulohenriquesouzabr@outlook.com>
2023-11-12 14:08:13 -0300

    teste commit feito vscode

--- GerenciaLocacao.java
+++ GerenciaLocacao.java
@@ -139,7 +139,7 @@ public class GerenciaLocacao {
   }
 
   public void buscarLocacao() {
-    System.out.print("Teste do paulotestes");
+    System.out.println("Esse commit foi feito no vscode.");
   }
 
 }
